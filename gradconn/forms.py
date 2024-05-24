02f5749ab300df4e66da71d1c47ea49107f2f995
Ermi24et <lilermi24@gmail.com>
2024-05-24 23:46:49 +0300

    forms

---
+++ gradconn/forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, IntegerField, TextAreaField, SelectField
+from wtforms.validators import Length, EqualTo, Email, InputRequired, ValidationError
+
+
+class SignUpForm(FlaskForm):
+    first_name = StringField('First Name', validators=[Length(min=3, max=30), InputRequired()])
+    last_name = StringField('Last Name', validators=[Length(min=3, max=30), InputRequired()])
+    email = StringField('Email', validators=[Email(), InputRequired()])
+    password = PasswordField('Password', validators=[Length(min=6), InputRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[EqualTo('password'), InputRequired()])
+    submit = SubmitField('Sign Up')
+
+class SignInForm(FlaskForm):
+    email = StringField('Email', validators=[Email(), InputRequired()])
+    password = PasswordField('Password', validators=[Length(min=6), InputRequired()])
+    submit = SubmitField('Sign In')
