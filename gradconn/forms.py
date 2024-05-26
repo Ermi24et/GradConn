1ec107d2e63cca4f3b3f69a69ec71e522850f462
Ermi24et <lilermi24@gmail.com>
2024-05-26 10:46:08 +0300

    added admin sign in and sign up forms

--- gradconn/forms.py
+++ gradconn/forms.py
@@ -15,3 +15,15 @@ class SignInForm(FlaskForm):
     email = StringField('Email', validators=[Email(), InputRequired()])
     password = PasswordField('Password', validators=[Length(min=6), InputRequired()])
     submit = SubmitField('Sign In')
+
+class AdminSignUpForm(FlaskForm):
+    name = StringField('Organization Name', validators=[Length(min=3, max=30), InputRequired()])
+    email = StringField('Email', validators=[Email(), InputRequired()])
+    password = PasswordField('Password', validators=[Length(min=6), InputRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[EqualTo('password'), InputRequired()])
+    submit = SubmitField('Sign Up')
+
+class AdminSignInForm(FlaskForm):
+    email = StringField('Email', validators=[Email(), InputRequired()])
+    password = PasswordField('Password', validators=[Length(min=6), InputRequired()])
+    submit = SubmitField('Sign In')
