7815acb90a00262cf3fceb4c7218bd57b0116ff2
Ermi24et <lilermi24@gmail.com>
2024-05-26 10:48:21 +0300

    added config files

--- gradconn/__init__.py
+++ gradconn/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from pymongo import MongoClient
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./templates', static_folder='./static')
 app.secret_key = "super secret key"
 
 client = MongoClient('localhost', 27017)
 
 db = client.flask_db
 gradconn = db.gradconn
+user_db = db.user_db
+admin_db = db.admin_db
 
 from gradconn import routes
