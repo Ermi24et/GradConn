843daca1692e5a385c9de4bbfdc20273178a56bf
Selomon1 <soya.vandy1950@gmail.com>
2024-05-28 21:38:27 +0300

    Employer dashboard

--- gradconn/forms.py
+++ gradconn/forms.py
@@ -27,3 +27,17 @@ class AdminSignInForm(FlaskForm):
     email = StringField('Email', validators=[Email(), InputRequired()])
     password = PasswordField('Password', validators=[Length(min=6), InputRequired()])
     submit = SubmitField('Sign In')
+
+class JobForm(FlaskForm):
+    position_title = StringField('Position Title', validators=[DataRequired()])
+    contract_type = StringField('Contract Type', validators=[DataRequired()])
+    working_condition = SelectField('Working Condition', choices=[('Remote', 'Remote'), ('Hybrid', 'Hybrid'), ('On Site', 'On Site')], validators=[DataRequired()])
+    type_of_vacancy = SelectField('Type of Vacancy', choices=[('Volunteer', 'Volunteer'), ('Internship', 'Internship'), ('Fellowship', 'Fellowship')], validators=[DataRequired()])
+    organization_description = TextAreaField('Organization Description', validators=[DataRequired()])
+    job_description = TextAreaField('Job Description', validators=[DataRequired])
+    required_education_experience = TextAreaField('Required Education & Experience', validators=[DataRequired()])
+    skills = TextAreaField('Skills', validators=[DataRequired()])
+    how_to_apply = TextAreaField('How to Apply', validators=[DataRequired()])
+    disclaimer = TextAreaField('Disclaimer', validators=[DataRequired()])
+    deadline_date = DateField('Application Deadline', format='%Y-%m-%d', validators=[DataRequired()])
+    submit = SubmitField('Post Job')
